ea65e3555b8b6e716fe583d0a2a2c10e88e77db8
phdljr <phdljr@naver.com>
2024-01-08 20:24:15 +0900

    project init

---
+++ src/main/java/org/springeel/oneclickrecipe/OneClickRecipeApplication.java
@@ -0,0 +1,13 @@
+package org.springeel.oneclickrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneClickRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneClickRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springeel/oneclickrecipe/OneClickRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springeel.oneclickrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneClickRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
