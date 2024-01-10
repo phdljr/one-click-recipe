69f1de92d2a1ce749ff13499512ae4ec4731442f
gyutae <qoxowkd0716@gmail.com>
2024-01-10 23:48:31 +0900

    Feat: getUserOrder 메서드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
@@ -1,9 +1,11 @@
 package org.springeel.oneclickrecipe.domain.order.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.order.dto.service.OrderReadResponseDto;
 import org.springeel.oneclickrecipe.domain.order.entity.Order;
 import org.springeel.oneclickrecipe.domain.order.entity.OrderStatus;
 import org.springeel.oneclickrecipe.domain.order.repository.OrderRepository;
@@ -15,4 +17,5 @@ public interface OrderService {
 
     OrderCreateResponseDto createOrder(OrderCreateServiceRequestDto serviceRequestDto, User user);
 
+    List<OrderReadResponseDto> getUserOrders(Long userId);
 }
