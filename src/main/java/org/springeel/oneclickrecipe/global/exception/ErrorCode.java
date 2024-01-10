e3e85ef80accfd99b5465cef6580533b16400eff
phdljr <phdljr@naver.com>
2024-01-10 21:10:31 +0900

    [#19]Feat: ErrorCode 메소드 추가

--- src/main/java/org/springeel/oneclickrecipe/global/exception/ErrorCode.java
+++ src/main/java/org/springeel/oneclickrecipe/global/exception/ErrorCode.java
@@ -1,5 +1,12 @@
 package org.springeel.oneclickrecipe.global.exception;
 
+import org.springframework.http.HttpStatus;
+
 public interface ErrorCode {
 
+    String name();
+
+    HttpStatus getStatus();
+
+    String getMessage();
 }
