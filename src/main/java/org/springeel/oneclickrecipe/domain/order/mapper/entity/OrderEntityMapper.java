0202fe940096cda9099e1998bab95b32a6f846cd
gyutae <qoxowkd0716@gmail.com>
2024-01-10 19:31:56 +0900

    Feat: OrderEntityMapper 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/entity/OrderEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/entity/OrderEntityMapper.java
@@ -1,10 +1,11 @@
 package org.springeel.oneclickrecipe.domain.order.mapper.entity;
 
+import org.mapstruct.Mapper;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.order.entity.Order;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
+@Mapper(componentModel = "spring")
 public interface OrderEntityMapper {
-
     Order toEntity(OrderCreateServiceRequestDto dto, User user);
 }
