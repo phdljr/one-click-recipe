938a70809c34c9f14182bd1e59c33d5c852c755d
pangseon <chaosen0521@gmail.com>
2024-01-11 03:28:47 +0900

    Feat: 레시피 PK 값과 유저 FK 값을 활용하여 레시피 삭제 기능 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/service/impl/RecipeServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/service/impl/RecipeServiceImpl.java
@@ -3,12 +3,14 @@ package org.springeel.oneclickrecipe.domain.recipe.service.impl;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeDeleteServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
 import org.springeel.oneclickrecipe.domain.recipe.mapper.entity.RecipeEntityMapper;
 import org.springeel.oneclickrecipe.domain.recipe.repository.RecipeRepository;
 import org.springeel.oneclickrecipe.domain.recipe.service.RecipeService;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springeel.oneclickrecipe.domain.user.repository.UserRepository;
+import org.springeel.oneclickrecipe.global.exception.ErrorCode;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -21,5 +23,23 @@ public class RecipeServiceImpl implements RecipeService {
     public void createRecipe(final RecipeCreateServiceRequestDto requestDto, User user) {
         Recipe recipe = recipeEntityMapper.toRecipe(requestDto, user);
         recipeRepository.save(recipe);
+    }
+
+//    public void deleteRecipe(final RecipeDeleteServiceRequestDto requestDto, Long userId) {
+//        Recipe recipe = recipeRepository.findRecipeByIdAndUser_Id(requestDto.recipeId(), userId);
+//        if (recipe == null) {
+//            throw new RuntimeException("recipe 값이 없습니다");
+//        }
+//        recipeRepository.delete(recipe);
+//    }
+
+    public void deleteRecipe(final RecipeDeleteServiceRequestDto requestDto, User user) {
+        Recipe recipe = recipeRepository.findRecipeByIdAndUser_Id(
+            requestDto.recipeId(),
+            user.getId());
+        if (recipe == null) {
+            throw new RuntimeException("recipe 값이 없습니다");
+        }
+        recipeRepository.delete(recipe);
     }
 }
