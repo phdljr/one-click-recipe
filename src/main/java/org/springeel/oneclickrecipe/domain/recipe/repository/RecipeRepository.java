dc44e2523c02654001a3131a401c2480c1f292d4
pangseon <chaosen0521@gmail.com>
2024-01-11 03:32:17 +0900

    Feat: 사용자가 요청한 레시피 PK 값과 유저의 FK 값을 통해 레시피 엔티티를 찾는다

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/repository/RecipeRepository.java
@@ -1,7 +1,10 @@
 package org.springeel.oneclickrecipe.domain.recipe.repository;
 
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    Recipe findRecipeByIdAndUser_Id(Long recipeId, Long userId);
 }
