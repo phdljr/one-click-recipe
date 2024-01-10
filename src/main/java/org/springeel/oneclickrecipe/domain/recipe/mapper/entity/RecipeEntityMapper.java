9b1c7880b9f253fdaf034f7a6f4dee2b3f8f9046
pangseon <chaosen0521@gmail.com>
2024-01-11 02:02:05 +0900

    Feat: 레시피 생성 시 Service Dto에서 요청을 Entity 타입으로 변경할 Mapper 생성

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/mapper/entity/RecipeEntityMapper.java
@@ -0,0 +1,9 @@
+package org.springeel.oneclickrecipe.domain.recipe.mapper.entity;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants.ComponentModel;
+
+@Mapper(componentModel = ComponentModel.SPRING)
+public interface RecipeEntityMapper {
+
+}
