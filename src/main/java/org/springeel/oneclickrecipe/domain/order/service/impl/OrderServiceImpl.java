26f5000b0574827c5c4c12fd29e6f54f87a4426e
gyutae <qoxowkd0716@gmail.com>
2024-01-10 19:40:56 +0900

    Feat: Mapper를 이용한 객체 생성

--- src/main/java/org/springeel/oneclickrecipe/domain/order/service/impl/OrderServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/service/impl/OrderServiceImpl.java
@@ -4,8 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.order.entity.Order;
-import org.springeel.oneclickrecipe.domain.order.entity.OrderStatus;
-import org.springeel.oneclickrecipe.domain.order.mapper.OrderMapper;
+import org.springeel.oneclickrecipe.domain.order.mapper.entity.OrderEntityMapper;
 import org.springeel.oneclickrecipe.domain.order.repository.OrderRepository;
 import org.springeel.oneclickrecipe.domain.order.service.OrderService;
 import org.springeel.oneclickrecipe.domain.user.UserRepository;
@@ -18,25 +17,14 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final UserRepository userRepository;
-
+    private final OrderEntityMapper orderEntityMapper; // Mapper 추가
     @Override
     public Order createOrder(OrderCreateServiceRequestDto serviceRequestDto) {
 
         User user = userRepository.findById(serviceRequestDto.userId())
             .orElseThrow(() -> new EntityNotFoundException("해당 유저를 찾을 수 없습니다." + serviceRequestDto.userId()));
 
-        Order order = new Order(
-            serviceRequestDto.receiverName(),
-            serviceRequestDto.receiverPhoneNumber(),
-            serviceRequestDto.senderName(),
-            serviceRequestDto.senderPhoneNumber(),
-            serviceRequestDto.address(),
-            serviceRequestDto.addressDetail(),
-            serviceRequestDto.requirement(),
-            serviceRequestDto.totalPrice(),
-            OrderStatus.WAITING,
-            user
-        );
+        Order order = orderEntityMapper.toEntity(serviceRequestDto, user); // Mapper를 이용한 객체 생성
 
         return orderRepository.save(order);
     }
