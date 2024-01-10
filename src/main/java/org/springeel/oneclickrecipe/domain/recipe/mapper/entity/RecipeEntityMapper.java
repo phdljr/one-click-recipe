2291204076d98cb17d820ba6b1049b35db52b1e2
pangseon <chaosen0521@gmail.com>
2024-01-11 02:32:07 +0900

    Feat: 레시피 생성 시 사용할 Mapper 메서드 생성

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/mapper/entity/RecipeEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/mapper/entity/RecipeEntityMapper.java
@@ -2,8 +2,13 @@ package org.springeel.oneclickrecipe.domain.recipe.mapper.entity;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
+import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface RecipeEntityMapper {
+
+    Recipe toRecipe(RecipeCreateServiceRequestDto requestDto, User user);
 
 }
