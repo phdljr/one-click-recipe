232a685b2dd3699473aec84fe462bfe50d73534d
gyutae <qoxowkd0716@gmail.com>
2024-01-10 22:52:11 +0900

    Fix: git pull 오류 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
@@ -2,6 +2,7 @@ package org.springeel.oneclickrecipe.domain.order.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.order.entity.Order;
 import org.springeel.oneclickrecipe.domain.order.entity.OrderStatus;
@@ -10,31 +11,8 @@ import org.springeel.oneclickrecipe.domain.user.UserRepository;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class OrderService {
+public interface OrderService {
 
-    private final OrderRepository orderRepository;
-    private final UserRepository userRepository;
+    OrderCreateResponseDto createOrder(OrderCreateServiceRequestDto serviceRequestDto, User user);
 
-    public Order createOrder(OrderCreateServiceRequestDto serviceRequestDto) {
-
-        User user = userRepository.findById(serviceRequestDto.userId())
-            .orElseThrow(() -> new EntityNotFoundException("해당 유저를 찾을 수 없습니다." + serviceRequestDto.userId()));
-
-        Order order = new Order(
-            serviceRequestDto.receiverName(),
-            serviceRequestDto.receiverPhoneNumber(),
-            serviceRequestDto.senderName(),
-            serviceRequestDto.senderPhoneNumber(),
-            serviceRequestDto.address(),
-            serviceRequestDto.addressDetail(),
-            serviceRequestDto.requirement(),
-            serviceRequestDto.totalPrice(),
-            OrderStatus.WAITING,
-            user
-        );
-
-        return orderRepository.save(order);
-    }
 }
