aa1942233dc0f6d34918b313ae9f500474ec08f4
gyutae <qoxowkd0716@gmail.com>
2024-01-10 23:53:49 +0900

    Feat: findByUserId 메서드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/order/repository/OrderRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/repository/OrderRepository.java
@@ -5,7 +5,8 @@ import org.springeel.oneclickrecipe.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
     // 사용자 ID에 따라 주문 목록 찾는 기능
     List<Order> findAllByUserId(Long userId);
+
+    List<Order> findByUserId(Long userId);
 }
