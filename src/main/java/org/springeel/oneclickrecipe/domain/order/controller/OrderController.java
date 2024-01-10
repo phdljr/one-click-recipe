6cb6ca04e2629125a179d12c6c49df5a79733e42
gyutae <qoxowkd0716@gmail.com>
2024-01-10 14:51:22 +0900

    feat: OrderController 구현

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/controller/OrderController.java
@@ -0,0 +1,34 @@
+package org.springeel.oneclickrecipe.domain.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.order.dto.controller.OrderCreateControllerRequestDto;
+import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
+import org.springeel.oneclickrecipe.domain.order.entity.Order;
+import org.springeel.oneclickrecipe.domain.order.mapper.OrderMapper;
+import org.springeel.oneclickrecipe.domain.order.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class OrderController {
+
+    private final OrderMapper orderMapper;
+    private final OrderService orderService;
+
+    @PostMapping("/orders")
+    public ResponseEntity<OrderCreateResponseDto> createOrder(
+        @RequestBody OrderCreateControllerRequestDto requestDto
+    ) {
+        Long userId = 1L;
+        Order order = orderService.createOrder(orderMapper.toServiceDto(requestDto, userId));
+        OrderCreateResponseDto responseDto = orderMapper.toControllerResponseDto(order);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+}
--- src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/OrderMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/OrderMapper.java
@@ -1,7 +1,6 @@
 package org.springeel.oneclickrecipe.domain.order.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import org.springeel.oneclickrecipe.domain.order.dto.controller.OrderCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
