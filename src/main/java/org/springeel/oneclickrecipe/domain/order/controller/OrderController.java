02506593c84a471ee2dde1dbe57a4907d7a47786
gyutae <qoxowkd0716@gmail.com>
2024-01-10 21:09:02 +0900

    Feat: OrderController 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/order/controller/OrderController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/controller/OrderController.java
@@ -3,11 +3,12 @@ package org.springeel.oneclickrecipe.domain.order.controller;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.order.dto.controller.OrderCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
-import org.springeel.oneclickrecipe.domain.order.entity.Order;
-import org.springeel.oneclickrecipe.domain.order.mapper.OrderMapper;
-import org.springeel.oneclickrecipe.domain.order.service.impl.OrderServiceImpl;
-import org.springframework.http.HttpStatus;
+import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.order.mapper.dto.OrderDtoMapper;
+import org.springeel.oneclickrecipe.domain.order.service.OrderService;
+import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
-    private final OrderMapper orderMapper;
-    private final OrderServiceImpl orderService;
+    private final OrderDtoMapper orderDtoMapper;
+    private final OrderService orderService;
 
     @PostMapping("/orders")
     public ResponseEntity<OrderCreateResponseDto> createOrder(
-        @RequestBody OrderCreateControllerRequestDto requestDto
+        @RequestBody OrderCreateControllerRequestDto createControllerRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetailsImpl
     ) {
-        Long userId = 1L;
-        Order order = orderService.createOrder(orderMapper.toServiceDto(requestDto, userId));
-        OrderCreateResponseDto responseDto = orderMapper.toControllerResponseDto(order);
-        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+        OrderCreateServiceRequestDto serviceRequestDto = orderDtoMapper.toOrderServiceRequestDto(
+            createControllerRequestDto);
+        OrderCreateResponseDto responseDto = orderService.createOrder(serviceRequestDto, userDetailsImpl.getUser());
+        return ResponseEntity.ok(responseDto);
     }
-
 }
