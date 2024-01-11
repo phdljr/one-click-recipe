bd88adf6e175a52f60af3e984ee4cd566e225d1c
pangseon <chaosen0521@gmail.com>
2024-01-12 01:23:24 +0900

    Feat: 조리과정 삭제 기능 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/service/impl/RecipeProcessServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/service/impl/RecipeProcessServiceImpl.java
@@ -8,6 +8,8 @@ import org.springeel.oneclickrecipe.domain.recipe.exception.RecipeErrorCode;
 import org.springeel.oneclickrecipe.domain.recipe.repository.RecipeRepository;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.entity.RecipeProcess;
+import org.springeel.oneclickrecipe.domain.recipeprocess.exception.NotFoundRecipeProcessException;
+import org.springeel.oneclickrecipe.domain.recipeprocess.exception.RecipeProcessErrorCode;
 import org.springeel.oneclickrecipe.domain.recipeprocess.mapper.entity.RecipeProcessEntityMapper;
 import org.springeel.oneclickrecipe.domain.recipeprocess.repository.RecipeProcessRepository;
 import org.springeel.oneclickrecipe.domain.recipeprocess.service.RecipeProcessService;
@@ -31,5 +33,18 @@ public class RecipeProcessServiceImpl implements RecipeProcessService {
             .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
         RecipeProcess recipeProcess = recipeProcessEntityMapper.toRecipeProcess(requestDto, recipe);
         recipeProcessRepository.save(recipeProcess);
+    }
+
+    public void deleteRecipeProcess(
+        Long recipeId,
+        User user,
+        Long processId
+    ) {
+        Recipe recipe = recipeRepository.findByIdAndUser(recipeId, user)
+            .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
+        RecipeProcess recipeProcess = recipeProcessRepository.findByIdAndRecipe(processId, recipe)
+            .orElseThrow(() -> new NotFoundRecipeProcessException(
+                RecipeProcessErrorCode.NOT_FOUND_RECIPE_PROCESS));
+        recipeProcessRepository.delete(recipeProcess);
     }
 }
