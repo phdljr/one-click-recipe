b09a69be38e3124a15b8b76778520ed7f1cee4e1
pangseon <chaosen0521@gmail.com>
2024-01-11 21:57:46 +0900

    Feat: 조리과정 Repository 생성

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/repository/RecipeProcessRepository.java
@@ -0,0 +1,8 @@
+package org.springeel.oneclickrecipe.domain.recipeprocess.repository;
+
+import org.springeel.oneclickrecipe.domain.recipeprocess.entity.RecipeProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeProcessRepository extends JpaRepository<RecipeProcess, Long> {
+
+}
