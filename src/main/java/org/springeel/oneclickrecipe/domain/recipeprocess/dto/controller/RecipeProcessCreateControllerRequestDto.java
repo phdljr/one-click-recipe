3aff1414c199c1e813bf70171d4057ebc8821821
pangseon <chaosen0521@gmail.com>
2024-01-11 23:24:49 +0900

    Feat: 조리과정 생성을 위한 요청 데이터 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/dto/controller/RecipeProcessCreateControllerRequestDto.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/dto/controller/RecipeProcessCreateControllerRequestDto.java
@@ -2,7 +2,8 @@ package org.springeel.oneclickrecipe.domain.recipeprocess.dto.controller;
 
 public record RecipeProcessCreateControllerRequestDto(
     String sequence,
-    String description
+    String description,
+    String imageUrl
 ) {
 
 }
