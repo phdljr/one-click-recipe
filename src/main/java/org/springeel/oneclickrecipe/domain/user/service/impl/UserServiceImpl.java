827df9c5bd44cf029c6735451bcef26e7e3b0ed2
phdljr <phdljr@naver.com>
2024-01-11 16:09:41 +0900

    [#34]Feat: 회원가입 Builder -> Mapper 로 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/user/service/impl/UserServiceImpl.java
@@ -2,15 +2,19 @@ package org.springeel.oneclickrecipe.domain.user.service.impl;
 
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import org.springeel.oneclickrecipe.domain.user.dto.service.UserCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.user.dto.service.UserLoginServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.user.dto.service.UserSignUpServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springeel.oneclickrecipe.domain.user.entity.UserRole;
 import org.springeel.oneclickrecipe.domain.user.exception.AlreadyExistsEmailException;
 import org.springeel.oneclickrecipe.domain.user.exception.DuplicateNicknameException;
+import org.springeel.oneclickrecipe.domain.user.exception.NotFoundUserException;
 import org.springeel.oneclickrecipe.domain.user.exception.NotMatchPasswordException;
 import org.springeel.oneclickrecipe.domain.user.exception.UserErrorCode;
+import org.springeel.oneclickrecipe.domain.user.mapper.entity.UserEntityMapper;
 import org.springeel.oneclickrecipe.domain.user.repository.UserRepository;
 import org.springeel.oneclickrecipe.domain.user.service.UserService;
+import org.springeel.oneclickrecipe.global.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +22,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserEntityMapper userEntityMapper;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Override
-    public void signUp(final UserCreateServiceRequestDto serviceRequestDto) {
+    public void signUp(final UserSignUpServiceRequestDto serviceRequestDto) {
         // 이메일 중복 체크
-        if(userRepository.existsByEmail(serviceRequestDto.email())){
+        if (userRepository.existsByEmail(serviceRequestDto.email())) {
             throw new AlreadyExistsEmailException(UserErrorCode.ALREADY_EXIST_EMAIL);
         }
 
@@ -34,18 +39,30 @@ public class UserServiceImpl implements UserService {
         }
 
         // 비밀번호가 같은지 확인
-        if(!Objects.equals(serviceRequestDto.password(), serviceRequestDto.passwordConfirm())){
+        if (!Objects.equals(serviceRequestDto.password(), serviceRequestDto.confirmPassword())) {
             throw new NotMatchPasswordException(UserErrorCode.NOT_MATCH_PASSWORD);
         }
 
-        // 저장
-        User user = User.builder()
-            .email(serviceRequestDto.email())
-            .nickname(serviceRequestDto.nickname())
-            .password(passwordEncoder.encode(serviceRequestDto.password()))
-            .role(UserRole.USER)
-            .build();
+//        // 저장
+//        User user = User.builder()
+//            .email(serviceRequestDto.email())
+//            .nickname(serviceRequestDto.nickname())
+//            .password(passwordEncoder.encode(serviceRequestDto.password()))
+//            .role(UserRole.USER)
+//            .build();
+
+        User user = userEntityMapper.toUser(serviceRequestDto, UserRole.USER);
 
         userRepository.save(user);
+    }
+
+    @Override
+    public void login(final UserLoginServiceRequestDto serviceRequestDto) {
+        User user = userRepository.findByEmail(serviceRequestDto.email())
+            .orElseThrow(() -> new NotFoundUserException(UserErrorCode.BAD_LOGIN));
+
+        if (!passwordEncoder.matches(serviceRequestDto.password(), user.getPassword())) {
+            throw new NotMatchPasswordException(UserErrorCode.BAD_LOGIN);
+        }
     }
 }
