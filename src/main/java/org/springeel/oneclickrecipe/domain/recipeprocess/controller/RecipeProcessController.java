faafa17ae19a15d24b89989c5508a794ef5bec3a
pangseon <chaosen0521@gmail.com>
2024-01-12 01:24:20 +0900

    Feat: 조리과정 삭제 dto 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
@@ -9,6 +9,7 @@ import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,5 +34,15 @@ public class RecipeProcessController {
             processDtoMapper.toRecipeProcessCreateServiceRequestDto(controllerRequestDto);
         processService.createRecipeProcess(serviceRequestDto, userDetails.user(), recipeId);
         return ResponseEntity.status(HttpStatus.CREATED).body(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{recipeId}/{processId}")
+    public ResponseEntity<?> delete(
+        @PathVariable Long recipeId,
+        @PathVariable Long processId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        processService.deleteRecipeProcess(recipeId, userDetails.user(), processId);
+        return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK);
     }
 }
