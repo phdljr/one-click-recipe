9954bc15b8423937d2c0b9f03891cdc9e756ee95
doyoon <doyoun0527@naver.com>
2024-01-11 14:51:41 +0900

    Feat: 후기 수정 기능 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/repository/RecipeRepository.java
@@ -3,11 +3,7 @@ package org.springeel.oneclickrecipe.domain.recipe.repository;
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
-    List<Recipe> findByRecipeId(Long recipeId);
 
 
 }
