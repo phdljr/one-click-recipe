ac043c66dfa7d53b34828d309fdb9a7f910c55ea
gyutae <qoxowkd0716@gmail.com>
2024-01-11 16:08:50 +0900

    Feat: 객체 매핑

--- src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
@@ -1,11 +1,15 @@
 package org.springeel.oneclickrecipe.domain.orderdetail.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springeel.oneclickrecipe.domain.orderdetail.dto.OrderDetailDto;
 import org.springeel.oneclickrecipe.domain.orderdetail.entity.OrderDetail;
 
 @Mapper(componentModel = "spring")
 public interface OrderDetailMapper {
-
+    @Mapping(source = "id", target = "detailId")
+    @Mapping(source = "foodName", target = "name")
+    @Mapping(source = "amount", target = "quantity")
+    @Mapping(source = "price", target = "unitPrice")
     OrderDetailDto toOrderDetailDto(OrderDetail orderDetail);
 }
