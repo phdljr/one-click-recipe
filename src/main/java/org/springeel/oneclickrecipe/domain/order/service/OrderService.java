b9ed202537a039f7e6346621fabcfb65d27fb8ce
gyutae <qoxowkd0716@gmail.com>
2024-01-12 02:04:31 +0900

    Feat: getAllUserOrders 매서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/service/OrderService.java
@@ -3,6 +3,7 @@ package org.springeel.oneclickrecipe.domain.order.service;
 import java.util.List;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.order.dto.service.OrderReadAllResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderReadResponseDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
@@ -13,4 +14,6 @@ public interface OrderService {
     List<OrderReadResponseDto> getUserOrders(Long userId);
 
     OrderReadResponseDto getOrderById(Long orderId, Long userId);
+
+    List<OrderReadAllResponseDto> getAllUserOrders(Long userId);
 }
