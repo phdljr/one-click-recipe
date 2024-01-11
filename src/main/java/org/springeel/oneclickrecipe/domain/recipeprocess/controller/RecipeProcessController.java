265df019feee701d55adecc276f06d836d5196a0
pangseon <chaosen0521@gmail.com>
2024-01-11 23:32:05 +0900

    Feat: 조리과정 생성 컨트롤러 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
@@ -1,6 +1,17 @@
 package org.springeel.oneclickrecipe.domain.recipeprocess.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.recipeprocess.dto.controller.RecipeProcessCreateControllerRequestDto;
+import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipeprocess.mapper.dto.RecipeProcessDtoMapper;
+import org.springeel.oneclickrecipe.domain.recipeprocess.service.RecipeProcessService;
+import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class RecipeProcessController {
 
+    private final RecipeProcessService processService;
+    private final RecipeProcessDtoMapper processDtoMapper;
+
+    @PostMapping("/{recipeId}/process")
+    public ResponseEntity<?> create(
+        @RequestBody RecipeProcessCreateControllerRequestDto controllerRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable Long recipeId
+    ) {
+        RecipeProcessCreateServiceRequestDto serviceRequestDto =
+            processDtoMapper.toRecipeProcessCreateServiceRequestDto(controllerRequestDto);
+        processService.createRecipeProcess(serviceRequestDto, userDetails.user(), recipeId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpStatus.CREATED);
+    }
 }
