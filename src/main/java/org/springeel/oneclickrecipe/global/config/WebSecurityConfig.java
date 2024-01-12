c8459328da9081ddb6f7c455f425e0071a69ac9f
phdljr <phdljr@naver.com>
2024-01-12 19:50:59 +0900

    [#67]Feat: CORS 설정

--- src/main/java/org/springeel/oneclickrecipe/global/config/WebSecurityConfig.java
+++ src/main/java/org/springeel/oneclickrecipe/global/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.springeel.oneclickrecipe.global.config;
 
+import java.util.Arrays;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.global.filter.JwtAuthorizationFilter;
 import org.springeel.oneclickrecipe.global.security.UserDetailsServiceImpl;
@@ -9,17 +11,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private static final long CORS_MAX_AGE_SEC = 86400;
 
     private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
@@ -34,21 +41,39 @@ public class WebSecurityConfig {
         return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
     }
 
+    private CorsConfigurationSource configurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:5173", // local
+                "https://one-click-recipe.netlify.app", // production view
+                "https://develop--one-click-recipe.netlify.app", // deploy view
+                "https://*--one-click-recipe.netlify.app" // pr view
+            ));
+            config.setAllowCredentials(true);
+            config.setMaxAge(CORS_MAX_AGE_SEC);
+            return config;
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable);
-
-        http.sessionManagement((sessionManagement) ->
-            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+            .cors(cors -> cors.configurationSource(configurationSource()))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
             authorizeHttpRequests
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("/api/v1/tests", "/api/v1/tests/**").permitAll()
                 .requestMatchers("/api/v1/users/login").permitAll()
                 .requestMatchers("/api/v1/users/signup").permitAll()
                 .anyRequest().authenticated()
