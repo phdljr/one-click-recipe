1b89c39c2d112e4f7c66e08564d0ab8daddbaf1d
gyutae <qoxowkd0716@gmail.com>
2024-01-12 11:42:46 +0900

    Feat: TEST -> ORDER로 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/order/exception/OrderErrorCode.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/exception/OrderErrorCode.java
@@ -10,13 +10,13 @@ import org.springframework.http.HttpStatus;
 public enum OrderErrorCode implements ErrorCode {
 
     // 400
-    ALREADY_EXIST_TEST(HttpStatus.BAD_REQUEST, "이미 주문내역이 존재합니다."),
+    ALREADY_EXIST_ORDER(HttpStatus.BAD_REQUEST, "이미 주문내역이 존재합니다."),
 
     // 403
-    FORBIDDEN_ACCESS_TEST(HttpStatus.FORBIDDEN, "주문내역에 접근할 수 없습니다."),
+    FORBIDDEN_ACCESS_ORDER(HttpStatus.FORBIDDEN, "주문내역에 접근할 수 없습니다."),
 
     // 404
-    NOT_FOUND_TEST(HttpStatus.NOT_FOUND, "주문내역를 찾을 수 없습니다.");
+    NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "주문내역를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
