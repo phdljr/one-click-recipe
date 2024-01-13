8806ac574001aeea082f9a291626e19174c748e0
pangseon <chaosen0521@gmail.com>
2024-01-14 01:19:35 +0900

    Feature: S3Provider에서 사용할 amazons3 bean 등록을 위해 @Configuration

--- src/main/java/org/springeel/oneclickrecipe/global/config/S3Config.java
+++ src/main/java/org/springeel/oneclickrecipe/global/config/S3Config.java
@@ -6,7 +6,9 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
