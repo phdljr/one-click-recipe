cee2a3677254fe1d0825275879632359a50d9e26
pangseon <chaosen0521@gmail.com>
2024-01-14 00:32:02 +0900

    Feature: 레시피_업데이트를 위한 Controller 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/controller/RecipeFoodController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/controller/RecipeFoodController.java
@@ -2,7 +2,9 @@ package org.springeel.oneclickrecipe.domain.recipefood.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.recipefood.dto.controller.RecipeFoodCreateControllerRequestDto;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.controller.RecipeFoodUpdateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipefood.mapper.dto.RecipeFoodDtoMapper;
 import org.springeel.oneclickrecipe.domain.recipefood.service.RecipeFoodService;
 import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
@@ -12,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +46,20 @@ public class RecipeFoodController {
         @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         recipeFoodService.deleteRecipeFood(recipeId, recipeFoodId, userDetails.user());
+        return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK);
+    }
+
+    @PutMapping("{recipeId}/recipeFoods/{recipeFoodId}")
+    public ResponseEntity<?> update(
+        @PathVariable Long recipeId,
+        @PathVariable Long recipeFoodId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestBody RecipeFoodUpdateControllerRequestDto requestDto
+    ) {
+        RecipeFoodUpdateServiceRequestDto serviceRequestDto =
+            recipeFoodDtoMapper.toRecipeFoodUpdateServiceRequestDto(requestDto);
+        recipeFoodService.updateRecipeFood(recipeId, recipeFoodId, userDetails.user(),
+            serviceRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK);
     }
 
