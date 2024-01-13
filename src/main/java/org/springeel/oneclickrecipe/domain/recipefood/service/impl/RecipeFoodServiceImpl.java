1817867c3e4323505a5679d779289d24af9e6ee9
pangseon <chaosen0521@gmail.com>
2024-01-13 19:30:19 +0900

    Feat: RecipeFood 데이터를 저장하는 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/impl/RecipeFoodServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/impl/RecipeFoodServiceImpl.java
@@ -1,5 +1,37 @@
 package org.springeel.oneclickrecipe.domain.recipefood.service.impl;
 
-public class RecipeFoodServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.food.entity.Food;
+import org.springeel.oneclickrecipe.domain.food.exception.FoodErrorCode;
+import org.springeel.oneclickrecipe.domain.food.exception.NotFoundFoodException;
+import org.springeel.oneclickrecipe.domain.food.repository.FoodRepository;
+import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.recipe.exception.NotFoundRecipeException;
+import org.springeel.oneclickrecipe.domain.recipe.exception.RecipeErrorCode;
+import org.springeel.oneclickrecipe.domain.recipe.repository.RecipeRepository;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipefood.entity.RecipeFood;
+import org.springeel.oneclickrecipe.domain.recipefood.mapper.service.RecipeFoodEntityMapper;
+import org.springeel.oneclickrecipe.domain.recipefood.repository.RecipeFoodRepository;
+import org.springeel.oneclickrecipe.domain.recipefood.service.RecipeFoodService;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
+public class RecipeFoodServiceImpl implements RecipeFoodService {
+
+    private final RecipeFoodRepository recipeFoodRepository;
+    private final FoodRepository foodRepository;
+    private final RecipeFoodEntityMapper recipeFoodEntityMapper;
+    private final RecipeRepository recipeRepository;
+
+    public void createRecipeFood(RecipeFoodCreateServiceRequestDto requestDto, Long recipeId) {
+        Food food = foodRepository.findByName(requestDto.foodName())
+            .orElseThrow(() -> new NotFoundFoodException(FoodErrorCode.NOT_FOUND_FOOD));
+        Recipe recipe = recipeRepository.findById(recipeId)
+            .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
+        RecipeFood recipeFood = recipeFoodEntityMapper.toRecipeFood(requestDto, food,
+            recipe);
+        recipeFoodRepository.save(recipeFood);
+    }
 }
