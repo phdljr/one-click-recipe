39409ef44a5941db17f6a7abcade5f1d8e34bede
pangseon <chaosen0521@gmail.com>
2024-01-13 23:25:20 +0900

    Feat: 레시피 식재료 삭제 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
@@ -1,9 +1,12 @@
 package org.springeel.oneclickrecipe.domain.recipefood.service;
 
 import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 public interface RecipeFoodService {
 
     void createRecipeFood(RecipeFoodCreateServiceRequestDto requestDto, Long recipeId);
+
+    void deleteRecipeFood(Long recipeFoodId, Long recipeId, User user);
 
 }
