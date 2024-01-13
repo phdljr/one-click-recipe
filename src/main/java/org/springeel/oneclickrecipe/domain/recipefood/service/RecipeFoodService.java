e0c54282b1b6978de3782ee3f6333fb4df183c3e
pangseon <chaosen0521@gmail.com>
2024-01-14 00:29:50 +0900

    Feature: 레시피 업데이트 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.recipefood.service;
 
 import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 public interface RecipeFoodService {
@@ -8,5 +9,8 @@ public interface RecipeFoodService {
     void createRecipeFood(RecipeFoodCreateServiceRequestDto requestDto, Long recipeId, User user);
 
     void deleteRecipeFood(Long recipeFoodId, Long recipeId, User user);
+
+    void updateRecipeFood(Long recipeId, Long recipeFoodId, User user,
+        RecipeFoodUpdateServiceRequestDto requestDto);
 
 }
