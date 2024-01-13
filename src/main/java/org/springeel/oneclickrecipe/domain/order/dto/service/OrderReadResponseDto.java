a262aa07bcf3888d57839a06431aed7a5a2158a7
gyutae <qoxowkd0716@gmail.com>
2024-01-14 02:12:26 +0900

    Comment: 주문 상세 정보 주석 처리

--- src/main/java/org/springeel/oneclickrecipe/domain/order/dto/service/OrderReadResponseDto.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/dto/service/OrderReadResponseDto.java
@@ -2,7 +2,6 @@ package org.springeel.oneclickrecipe.domain.order.dto.service;
 
 import java.util.List;
 import org.springeel.oneclickrecipe.domain.order.entity.OrderStatus;
-import org.springeel.oneclickrecipe.domain.orderdetail.dto.OrderDetailDto;
 
 public record OrderReadResponseDto(
     Long orderId,
@@ -11,8 +10,8 @@ public record OrderReadResponseDto(
     String address,
     String addressDetail,
     Integer totalPrice,
-    OrderStatus orderStatus,
-    List<OrderDetailDto> orderDetails // 주문 상세 정보를 담을 리스트
+    OrderStatus orderStatus
+//    List<OrderDetailDto> orderDetails // 주문 상세 정보를 담을 리스트
 ) {
 
 }
