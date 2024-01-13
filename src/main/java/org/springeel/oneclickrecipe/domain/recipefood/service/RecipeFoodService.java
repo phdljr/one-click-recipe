028998f9af699041155115369f018a5654db1888
pangseon <chaosen0521@gmail.com>
2024-01-13 19:30:45 +0900

    Feat: Controller에서 사용할 메서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/service/RecipeFoodService.java
@@ -1,5 +1,9 @@
 package org.springeel.oneclickrecipe.domain.recipefood.service;
 
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
+
 public interface RecipeFoodService {
+
+    void createRecipeFood(RecipeFoodCreateServiceRequestDto requestDto, Long recipeId);
 
 }
