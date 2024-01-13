ce27eaa9c0536e9d89b9ecec4daf194e3e3b7679
pangseon <chaosen0521@gmail.com>
2024-01-13 17:32:25 +0900

    Feat: 레시피 식재료 등록을 위한 Mapper 생성

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/mapper/dto/RecipeFoodDtoMapper.java
@@ -0,0 +1,5 @@
+package org.springeel.oneclickrecipe.domain.recipefood.mapper.dto;
+
+public interface RecipeFoodDtoMapper {
+
+}
---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/mapper/service/RecipeFoodServiceMapper.java
@@ -0,0 +1,5 @@
+package org.springeel.oneclickrecipe.domain.recipefood.mapper.service;
+
+public interface RecipeFoodServiceMapper {
+
+}
