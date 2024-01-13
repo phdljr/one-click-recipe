6479955c5dd5180599604fdb1c28c1c526d6b932
gyutae <qoxowkd0716@gmail.com>
2024-01-14 02:11:47 +0900

    Remove: 매핑 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/entity/OrderEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/mapper/entity/OrderEntityMapper.java
@@ -7,23 +7,23 @@ import org.springeel.oneclickrecipe.domain.order.dto.service.OrderCreateServiceR
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderReadAllResponseDto;
 import org.springeel.oneclickrecipe.domain.order.dto.service.OrderReadResponseDto;
 import org.springeel.oneclickrecipe.domain.order.entity.Order;
-import org.springeel.oneclickrecipe.domain.orderdetail.mapper.OrderDetailMapper;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
-@Mapper(componentModel = "spring", uses = {OrderDetailMapper.class})
+@Mapper(componentModel = "spring")
 public interface OrderEntityMapper {
 
     @Mapping(target = "status", expression = "java(OrderStatus.WAITING)")
     Order toEntity(OrderCreateServiceRequestDto dto, User user);
 
     /**
-     * @Mapping를 사용한 이유는 한 객체의 필드를 다른 객체의 필드로 매핑하기 위함
-     * source는 원본 객체의 필드 이름, target은 대상 객체의 필드 이름
+     * @Mapping를 사용한 이유는 한 객체의 필드를 다른 객체의 필드로 매핑하기 위함 source는 원본 객체의 필드 이름, target은 대상 객체의 필드 이름
      */
     @Mapping(source = "id", target = "orderId")
     @Mapping(source = "status", target = "orderStatus")
     OrderCreateResponseDto toResponseDto(Order order);
+
     @Mapping(target = "orderDetails", source = "orderDetails")
     OrderReadResponseDto toOrderReadResponseDto(Order order);
+
     OrderReadAllResponseDto toOrderReadAllResponseDto(Order order);
 }
