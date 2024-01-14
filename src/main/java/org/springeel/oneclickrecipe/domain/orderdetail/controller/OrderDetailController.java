e57a09e53cec0d51bdbb352382d8fa6fa2319190
gyutae <qoxowkd0716@gmail.com>
2024-01-14 19:25:36 +0900

    Feat: Url 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/controller/OrderDetailController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/controller/OrderDetailController.java
@@ -6,20 +6,23 @@ import org.springeel.oneclickrecipe.domain.orderdetail.dto.service.OrderDetailCr
 import org.springeel.oneclickrecipe.domain.orderdetail.service.OrderDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/order-details")
+@RequestMapping("/api/v1/users")
 @RestController
 public class OrderDetailController {
 
     private final OrderDetailService orderDetailService;
 
-    @PostMapping
+    @PostMapping("/{userId}/{orderId}/order-details")
     public ResponseEntity<OrderDetailCreateResponseDto> createOrderDetail(
+        @PathVariable Long userId,
+        @PathVariable Long orderId,
         @RequestBody OrderDetailCreateControllerRequestDto orderDetailCreateControllerRequestDto
     ) {
         OrderDetailCreateResponseDto orderDetailCreateResponseDto = orderDetailService.createOrderDetail(
