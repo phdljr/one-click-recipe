dc8b15dea95beb2431edb98586e56090df4b2fe7
pangseon <chaosen0521@gmail.com>
2024-01-15 03:30:02 +0900

    Refactor: S3에연결을 위한 region 값 변경

--- src/main/java/org/springeel/oneclickrecipe/global/config/S3Config.java
+++ src/main/java/org/springeel/oneclickrecipe/global/config/S3Config.java
@@ -17,7 +17,7 @@ public class S3Config {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${cloud.aws.region}")
     private String region;
 
     @Bean
