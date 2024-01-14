4fa447242cefb34d8e873364258f7821226e4d55
gyutae <qoxowkd0716@gmail.com>
2024-01-14 17:43:00 +0900

    Feat: orderId 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/dto/controller/OrderDetailCreateControllerRequestDto.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/dto/controller/OrderDetailCreateControllerRequestDto.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.orderdetail.dto.controller;
 
 public record OrderDetailCreateControllerRequestDto(
+    Long orderId,
     String foodName,
     Short amount,
     Integer price
