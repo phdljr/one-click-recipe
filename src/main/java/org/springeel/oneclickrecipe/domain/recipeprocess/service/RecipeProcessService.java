266b31812b70c83d8a4cb877cc970bb987cc40c4
pangseon <chaosen0521@gmail.com>
2024-01-15 03:38:50 +0900

    Refactor: 레시피_조리과정의 imageUrl에 사용자가 업로드한 파일을 S3에 저장 후 저장 된 경로를 반환하는 메서드 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/service/RecipeProcessService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/service/RecipeProcessService.java
@@ -1,13 +1,15 @@
 package org.springeel.oneclickrecipe.domain.recipeprocess.service;
 
+import java.io.IOException;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface RecipeProcessService {
 
     void createRecipeProcess(RecipeProcessCreateServiceRequestDto requestDto, User user,
-        Long recipe_Id);
+        Long recipe_Id, MultipartFile multipartFile) throws IOException;
 
     void deleteRecipeProcess(Long recipeId, User user, Long processId);
 
