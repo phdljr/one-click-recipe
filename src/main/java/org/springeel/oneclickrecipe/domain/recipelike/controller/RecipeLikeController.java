10891974d8498d6eddbd42db5564f413f8781ebc
doyoon <doyoun0527@naver.com>
2024-01-15 19:11:26 +0900

    Feat: RecipeLikeController 구현(좋아요 삭제)

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/controller/RecipeLikeController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/controller/RecipeLikeController.java
@@ -6,6 +6,8 @@ import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCrea
 import org.springeel.oneclickrecipe.domain.recipelike.mapper.dto.RecipeLikeDtoMapper;
 import org.springeel.oneclickrecipe.domain.recipelike.service.RecipeLikeService;
 import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,4 +30,12 @@ public class RecipeLikeController {
         recipeLikeService.create(userDetails.user(), serviceRequestDto, recipeId);
     }
 
+    @DeleteMapping("/{recipeId}/likes") //좋아요 삭제
+    public ResponseEntity<Void> delete(
+        @PathVariable(name = "recipeId") Long recipeId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        recipeLikeService.deleteRecipeLike(userDetails.user(), recipeId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
