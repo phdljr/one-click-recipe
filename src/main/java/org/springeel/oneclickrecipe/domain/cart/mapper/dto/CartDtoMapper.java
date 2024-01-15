c14f13243bf886a23f6ed0ea4595063cc5dc00f8
gyutae <qoxowkd0716@gmail.com>
2024-01-15 21:35:37 +0900

    Refactor: CartDtoMapper 코드 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/mapper/dto/CartDtoMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/mapper/dto/CartDtoMapper.java
@@ -3,14 +3,10 @@ package org.springeel.oneclickrecipe.domain.cart.mapper.dto;
 import org.mapstruct.Mapper;
 import org.springeel.oneclickrecipe.domain.cart.dto.controller.CartCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.cart.dto.service.CartCreateServiceRequestDto;
-import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
 
 @Mapper(componentModel = "spring")
 public interface CartDtoMapper {
 
-    // Controller용 DTO에서 Entity로 변환
-    Cart dtoToEntity(CartCreateControllerRequestDto dto);
-
-    // Service용 DTO에서 Entity로 변환
-    Cart dtoToEntity(CartCreateServiceRequestDto dto);
+    CartCreateServiceRequestDto toServiceRequestDto(
+        CartCreateControllerRequestDto cartCreateControllerRequestDto);
 }
