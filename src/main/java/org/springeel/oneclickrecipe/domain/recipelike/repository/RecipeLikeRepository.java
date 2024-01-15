2c9bfb4c21f538c556de8bbd0cea50715a183ff9
doyoon <doyoun0527@naver.com>
2024-01-16 00:50:25 +0900

    Feat: Long -> RecipeLikeId(복합키)

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/repository/RecipeLikeRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/repository/RecipeLikeRepository.java
@@ -1,8 +1,9 @@
 package org.springeel.oneclickrecipe.domain.recipelike.repository;
 
 import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLike;
+import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecipeLikeRepository extends JpaRepository<RecipeLike, Long> {
+public interface RecipeLikeRepository extends JpaRepository<RecipeLike, RecipeLikeId> {
 
 }
