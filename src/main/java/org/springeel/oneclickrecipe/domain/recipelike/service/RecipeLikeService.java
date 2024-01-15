9081e1ea4880e88951d9a9dd8393c85522538bb0
doyoon <doyoun0527@naver.com>
2024-01-16 00:58:23 +0900

    Feat: Service에 있는 DTO 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/RecipeLikeService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/RecipeLikeService.java
@@ -1,11 +1,8 @@
 package org.springeel.oneclickrecipe.domain.recipelike.service;
 
-import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 public interface RecipeLikeService {
-
-    void create(User user, RecipeLikeCreateServiceRequestDto serviceRequestDto, Long recipeId);
-
-    void delete(User user, Long recipeId);
+    void create(User user, Long recipeId);
+    //  void delete(User user, Long recipeId);
 }
