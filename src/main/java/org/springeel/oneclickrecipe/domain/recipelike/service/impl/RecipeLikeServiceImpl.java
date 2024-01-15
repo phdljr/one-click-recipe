cdc266839d62fd644a594b5e1dc07ba1ddc471ea
doyoon <doyoun0527@naver.com>
2024-01-15 15:09:04 +0900

    Feat: RecipeLikeServiceImpl(레시피 좋아요 생성)구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/impl/RecipeLikeServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/impl/RecipeLikeServiceImpl.java
@@ -1,7 +1,13 @@
 package org.springeel.oneclickrecipe.domain.recipelike.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.recipe.exception.NotFoundRecipeException;
+import org.springeel.oneclickrecipe.domain.recipe.exception.RecipeErrorCode;
+import org.springeel.oneclickrecipe.domain.recipe.repository.RecipeRepository;
 import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLike;
+import org.springeel.oneclickrecipe.domain.recipelike.mapper.entity.RecipeLikeEntityMapper;
 import org.springeel.oneclickrecipe.domain.recipelike.repository.RecipeLikeRepository;
 import org.springeel.oneclickrecipe.domain.recipelike.service.RecipeLikeService;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
@@ -12,10 +18,13 @@ import org.springframework.stereotype.Service;
 public class RecipeLikeServiceImpl implements RecipeLikeService {
 
     private final RecipeLikeRepository recipeLikeRepository;
-
+    private final RecipeRepository recipeRepository;
+    private final RecipeLikeEntityMapper recipeLikeEntityMapper;
 
     @Override
     public void create(User user, RecipeLikeCreateServiceRequestDto serviceRequestDto, Long recipeId) {
-
+        Recipe recipe = recipeRepository.findById(recipeId)
+            .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
+        RecipeLike recipeLike = recipeLikeEntityMapper.toRecipeLike(serviceRequestDto, user, recipe);
     }
 }
