12461951694e7137d292ef00fb31aaef8ed240cf
doyoon <doyoun0527@naver.com>
2024-01-15 14:33:30 +0900

    Feat:RecipeLikeDtoMapper 구현(mapper 어노테이션 추가)

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/mapper/dto/RecipeLikeDtoMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/mapper/dto/RecipeLikeDtoMapper.java
@@ -1,7 +1,11 @@
 package org.springeel.oneclickrecipe.domain.recipelike.mapper.dto;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.springeel.oneclickrecipe.domain.recipelike.dto.controller.RecipeLikeCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 
 public interface RecipeLikeDtoMapper {
     RecipeLikeCreateServiceRequestDto toRecipeLikeCreateServiceRequestDto(
