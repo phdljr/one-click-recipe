cb16f9038ff119090125cc045c1e4023209861c0
doyoon <doyoun0527@naver.com>
2024-01-15 15:08:33 +0900

    Feat: RecipeLikeEntityMapper 구현

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/mapper/entity/RecipeLikeEntityMapper.java
@@ -0,0 +1,14 @@
+package org.springeel.oneclickrecipe.domain.recipelike.mapper.entity;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLike;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+
+public interface RecipeLikeEntityMapper {
+    RecipeLike toRecipeLike(RecipeLikeCreateServiceRequestDto serviceRequestDto, User user, Recipe recipe);
+}
