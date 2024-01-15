6a92d0e33bb84ffbf6792290c2ce47083f9e33eb
doyoon <doyoun0527@naver.com>
2024-01-16 00:51:17 +0900

    Feat: 쓰이지 않는 예외 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/exception/RecipeLikeErrorCode.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/exception/RecipeLikeErrorCode.java
@@ -12,9 +12,6 @@ public enum RecipeLikeErrorCode implements ErrorCode {
     //400
     ALREADY_EXIST_RECIPELIKE(HttpStatus.BAD_REQUEST, "이미 좋아요가 존재합니다."),
 
-    //403
-    FORBIDDEN_ACCESS_RECIPELIKE(HttpStatus.FORBIDDEN, "레시피에 접근할 수 없습니다."),
-
     //404
     NOT_FOUND_RECIPELIKE(HttpStatus.NOT_FOUND, "레시피를 찾을 수 없습니다.");
 
