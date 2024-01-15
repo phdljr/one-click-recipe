317621fe321125315d1d5a023c8ea729d63d7399
doyoon <doyoun0527@naver.com>
2024-01-16 01:34:20 +0900

    Feat: RecipeLikeDtoMapper 에 있는 DTO 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/mapper/dto/RecipeLikeDtoMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/mapper/dto/RecipeLikeDtoMapper.java
@@ -2,12 +2,9 @@ package org.springeel.oneclickrecipe.domain.recipelike.mapper.dto;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.springeel.oneclickrecipe.domain.recipelike.dto.controller.RecipeLikeCreateControllerRequestDto;
-import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 
 public interface RecipeLikeDtoMapper {
-    RecipeLikeCreateServiceRequestDto toRecipeLikeCreateServiceRequestDto(
-        RecipeLikeCreateControllerRequestDto createControllerRequestDto);
+
 }
