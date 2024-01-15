998e3a29ea71e66451415146b1c69ffc377a9c7c
doyoon <doyoun0527@naver.com>
2024-01-15 14:45:02 +0900

    Feat: RecipeLikeService 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/RecipeLikeService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/service/RecipeLikeService.java
@@ -1,5 +1,9 @@
 package org.springeel.oneclickrecipe.domain.recipelike.service;
 
+import org.springeel.oneclickrecipe.domain.recipelike.dto.service.RecipeLikeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
+
 public interface RecipeLikeService {
 
+    void create(User user, RecipeLikeCreateServiceRequestDto serviceRequestDto, Long recipeId);
 }
