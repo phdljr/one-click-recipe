db56daed476e05661de630b5cecc0e676a00f397
gyutae <qoxowkd0716@gmail.com>
2024-01-15 20:37:44 +0900

    Refactor: CartId로 값 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
@@ -1,8 +1,9 @@
 package org.springeel.oneclickrecipe.domain.cart.repository;
 
 import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
+import org.springeel.oneclickrecipe.domain.cart.entity.CartId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart, CartId> {
 
 }
