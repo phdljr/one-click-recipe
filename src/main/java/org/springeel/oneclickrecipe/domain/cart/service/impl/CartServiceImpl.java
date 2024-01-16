a9f50abc87da83ec048e2dd56b6993bba37a02fa
gyutae <qoxowkd0716@gmail.com>
2024-01-17 04:12:57 +0900

    Refactor: 로직 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/service/impl/CartServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/service/impl/CartServiceImpl.java
@@ -41,16 +41,7 @@ public class CartServiceImpl implements CartService {
     private void addCartItem(User user, Long recipeFoodId) {
         RecipeFood recipeFood = recipeFoodRepository.findById(recipeFoodId)
             .orElseThrow(() -> new NotFoundCartException(CartErrorCode.NOT_FOUND_CART));
-        /*
-         * 이미 동일한 아이템이 존재하는지 확인하는 로직입니다.
-         * 장바구니에 아이템 추가 전에, 장바구니 초기화 로직을 넣었으나
-         * 혹시 몰라 넣은 예외처리 코드입니다.
-         */
-        boolean exists = cartRepository.findByUserAndRecipeFood(user, recipeFood)
-            .isPresent();
-        if (exists) {
-            throw new AlreadyExistsCartException(CartErrorCode.ALREADY_EXIST_CART);
-        }
+
         // 장바구니 아이템 추가
         Cart cartItem = new Cart(user, recipeFood);
         cartRepository.save(cartItem);
