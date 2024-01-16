f11f90ef59dbf7ca8c5124fe2cd73fb29ba3c020
doyoon <doyoun0527@naver.com>
2024-01-17 02:56:28 +0900

    Feat: FollowServiceImpl 에 어노테이션(@Service, @RequiredArgsConstructor) 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
@@ -1,8 +1,12 @@
 package org.springeel.oneclickrecipe.domain.follow.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class FollowServiceImpl implements FollowService {
 
     @Override
