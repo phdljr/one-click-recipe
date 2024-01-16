056406d32deb48d20a9c1f310f43c5ebdbdba234
doyoon <doyoun0527@naver.com>
2024-01-17 02:56:51 +0900

    Feat: FollowController 구현(사용자 구독 생성)

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/controller/FollowController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/controller/FollowController.java
@@ -1,6 +1,13 @@
 package org.springeel.oneclickrecipe.domain.follow.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
+import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users/{userId}") //사용자 구독 생성
 
 public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("/subscribes/{subUserId}")
+    public ResponseEntity<Void> create(
+        @PathVariable(name = "userId") Long userId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        followService.create(userDetails.user(), userId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
