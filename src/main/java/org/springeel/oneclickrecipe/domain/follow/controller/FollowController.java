5a84b77f08055eee34037093b02f563befc690ab
doyoon <doyoun0527@naver.com>
2024-01-17 02:39:41 +0900

    Feat: 사용자 구독 생성 API 초기작성

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/controller/FollowController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/controller/FollowController.java
@@ -1,4 +1,12 @@
 package org.springeel.oneclickrecipe.domain.follow.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users/{userId}") //사용자 구독 생성
+
 public class FollowController {
 }
