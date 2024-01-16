a9e8eb76b61e03b814cb0d68271163c9aaca3c8e
doyoon <doyoun0527@naver.com>
2024-01-16 16:20:39 +0900

    Feat: RecipeLikeRepository 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipelike/repository/RecipeLikeRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipelike/repository/RecipeLikeRepository.java
@@ -1,9 +1,15 @@
 package org.springeel.oneclickrecipe.domain.recipelike.repository;
 
+import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
 import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLike;
 import org.springeel.oneclickrecipe.domain.recipelike.entity.RecipeLikeId;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface RecipeLikeRepository extends JpaRepository<RecipeLike, RecipeLikeId> {
     boolean existsByUserIdAndRecipeId(Long userId, Long recipeId);
+
+    Optional<Recipe> findByIdAndUser(User user, Long recipeId);
 }
