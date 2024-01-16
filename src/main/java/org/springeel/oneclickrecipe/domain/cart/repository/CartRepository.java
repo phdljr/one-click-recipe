4a8f303f1e0fe005180d7786eb3b608ace44adc2
gyutae <qoxowkd0716@gmail.com>
2024-01-16 19:19:44 +0900

    Feat: findByUserIdAndRecipeFoodId 매서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
 package org.springeel.oneclickrecipe.domain.cart.repository;
 
+import java.util.Optional;
 import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
 import org.springeel.oneclickrecipe.domain.cart.entity.CartId;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,7 @@ public interface CartRepository extends JpaRepository<Cart, CartId> {
 
     @Transactional
     void deleteByUserId(Long userId);
+
+    // 특정 사용자의 장바구니에서 특정 레시피_식재료를 찾는 메소드
+    Optional<Cart> findByUserIdAndRecipeFoodId(Long userId, Long recipeFoodId);
 }
