a649b1fe68b68a5fc7e7b0efd67f466f1c5d6b75
gyutae <qoxowkd0716@gmail.com>
2024-01-17 04:12:26 +0900

    Refactor: 매서드 수정

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
@@ -3,12 +3,14 @@ package org.springeel.oneclickrecipe.domain.cart.repository;
 import java.util.Optional;
 import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
 import org.springeel.oneclickrecipe.domain.cart.entity.CartId;
+import org.springeel.oneclickrecipe.domain.recipefood.entity.RecipeFood;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, CartId> {
 
-    void deleteByUserId(Long userId);
+    void deleteByUserId(User user);
 
     // 특정 사용자의 장바구니에서 특정 레시피_식재료를 찾는 메소드
-    Optional<Cart> findByUserIdAndRecipeFoodId(Long userId, Long recipeFoodId);
+    Optional<Cart> findByUserAndRecipeFood(User user, RecipeFood recipeFood);
 }
