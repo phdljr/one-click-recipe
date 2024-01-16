1238f54c730e227ae8310e8bc96930e4e9edc599
gyutae <qoxowkd0716@gmail.com>
2024-01-16 20:41:58 +0900

    Feat: addCartItem 매서드 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/service/CartService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/service/CartService.java
@@ -3,4 +3,6 @@ package org.springeel.oneclickrecipe.domain.cart.service;
 public interface CartService {
 
     void clearCart(Long userId);
+
+    void addCartItem(Long userId, Long recipeFoodId);
 }
