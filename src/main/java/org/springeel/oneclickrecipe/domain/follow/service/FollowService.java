b8334ad63ca9cedc0bcdbeccab283ba9facfaf94
doyoon <doyoun0527@naver.com>
2024-01-17 02:54:35 +0900

    Feat: FollowService 에 create 메서드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/service/FollowService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/FollowService.java
@@ -1,4 +1,7 @@
 package org.springeel.oneclickrecipe.domain.follow.service;
 
+import org.springeel.oneclickrecipe.domain.user.entity.User;
+
 public interface FollowService {
+    void create(User user, Long userId);
 }
