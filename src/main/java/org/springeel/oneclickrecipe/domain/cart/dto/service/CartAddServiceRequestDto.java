ca78d7030ecd46366bfe0cde718fb7a1ccc3f4c8
gyutae <qoxowkd0716@gmail.com>
2024-01-17 03:50:35 +0900

    Refactor: List로 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/dto/service/CartAddServiceRequestDto.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/dto/service/CartAddServiceRequestDto.java
@@ -1,8 +1,10 @@
 package org.springeel.oneclickrecipe.domain.cart.dto.service;
 
+import java.util.List;
+
 public record CartAddServiceRequestDto(
     Long userId,
-    Long recipeFoodId
+    List<Long> recipeFoodIds
 ) {
 
 }
