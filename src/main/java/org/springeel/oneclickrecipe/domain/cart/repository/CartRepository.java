9df54d979a34f16f1e11ac24d775c139110cc934
gyutae <qoxowkd0716@gmail.com>
2024-01-16 19:13:40 +0900

    Feat: @Repository 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
@@ -3,8 +3,10 @@ package org.springeel.oneclickrecipe.domain.cart.repository;
 import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
 import org.springeel.oneclickrecipe.domain.cart.entity.CartId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, CartId> {
 
     @Transactional
