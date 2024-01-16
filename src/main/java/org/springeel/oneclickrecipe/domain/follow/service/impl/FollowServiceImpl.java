8143ec9ea8ed4dbc3ebad5eb4eeea2cd91991b49
doyoon <doyoun0527@naver.com>
2024-01-17 02:55:37 +0900

    Feat: FollowServiceImpl 구현

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
@@ -0,0 +1,12 @@
+package org.springeel.oneclickrecipe.domain.follow.service.impl;
+
+import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
+import org.springeel.oneclickrecipe.domain.user.entity.User;
+
+public class FollowServiceImpl implements FollowService {
+
+    @Override
+    public void create(User user, Long userId) {
+
+    }
+}
