985097228019b28b903f203cf4dd08c1202a0fd4
doyoon <doyoun0527@naver.com>
2024-01-17 11:31:51 +0900

    Feat: FollowServiceImpl 에 FollowRepository 주입하기

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.follow.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.follow.repository.FollowRepository;
 import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class FollowServiceImpl implements FollowService {
+
+    private final FollowRepository followRepository;
 
     @Override
     public void create(User user, Long userId) {
