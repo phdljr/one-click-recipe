7743b7bd0cbcbe3a1d16725964b699e5c3e2c5f7
doyoon <doyoun0527@naver.com>
2024-01-17 11:31:00 +0900

    Feat: FollowRepository 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/repository/FollowRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/repository/FollowRepository.java
@@ -1,4 +1,8 @@
 package org.springeel.oneclickrecipe.domain.follow.repository;
 
-public interface FollowRepository {
+import org.springeel.oneclickrecipe.domain.follow.entity.Follow;
+import org.springeel.oneclickrecipe.domain.follow.entity.FollowId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, FollowId> {
 }
