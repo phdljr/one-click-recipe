ae000255398264cec61ec864adcc060476da8948
gyutae <qoxowkd0716@gmail.com>
2024-01-17 23:04:44 +0900

    Feat: CartService - getCart 메소드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/service/CartService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/service/CartService.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.cart.service;
 
 import java.util.List;
+import org.springeel.oneclickrecipe.domain.cart.dto.service.CartCheckResponseDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 public interface CartService {
@@ -8,4 +9,6 @@ public interface CartService {
     void clearCart(User user);
 
     void addCartItems(User user, List<Long> recipeFoodIds);
+
+    CartCheckResponseDto getCart(User user); // 장바구니 조회 메소드 추가
 }
