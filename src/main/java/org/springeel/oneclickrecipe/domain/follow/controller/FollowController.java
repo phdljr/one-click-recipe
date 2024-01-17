744dec8c799261c7c31b1691f12140696c616ffe
doyoon <doyoun0527@naver.com>
2024-01-18 01:02:44 +0900

    Feat: Follow 기능 수정 및 구현

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/controller/FollowController.java
@@ -0,0 +1,30 @@
+package org.springeel.oneclickrecipe.domain.follow.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
+import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("/subscribes/{subUserId}") //사용자 구독 생성
+    public ResponseEntity<Void> create(
+        @PathVariable(name = "subUserId") Long followerId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        followService.create(userDetails.user(), followerId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,11 @@
+package org.springeel.oneclickrecipe.domain.follow.repository;
+
+import org.springeel.oneclickrecipe.domain.follow.entity.Follow;
+import org.springeel.oneclickrecipe.domain.follow.entity.FollowId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, FollowId> {
+    Optional<Follow> findByFollowerId(Long followerId);
+}
