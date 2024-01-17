b8fbf114a99c300c66a534c0af9481b8b65c0aa2
gyutae <qoxowkd0716@gmail.com>
2024-01-17 22:40:24 +0900

    Feat: findByUser 메소드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
 package org.springeel.oneclickrecipe.domain.cart.repository;
 
+import java.util.List;
 import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
 import org.springeel.oneclickrecipe.domain.cart.entity.CartId;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CartRepository extends JpaRepository<Cart, CartId> {
 
     void deleteByUser(User user);
+
+    List<Cart> findByUser(User user);
 }
