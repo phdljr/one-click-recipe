c72258dce46ac4813f02710e0c0519ee93301bc6
gyutae <qoxowkd0716@gmail.com>
2024-01-18 18:54:51 +0900

    Feat: Order 객체 매핑

--- src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
@@ -12,5 +12,6 @@ public interface OrderDetailMapper {
     @Mapping(source = "cart.recipeFood.food.name", target = "foodName")
     @Mapping(source = "cart.recipeFood.amount", target = "amount")
     @Mapping(source = "cart.recipeFood.food.price", target = "price")
+    @Mapping(source = "order", target = "order")
     OrderDetail toOrderDetail(Cart cart, Order order);
 }
