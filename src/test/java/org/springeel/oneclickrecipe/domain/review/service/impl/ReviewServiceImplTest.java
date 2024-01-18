95da8308306d9e5a0d4c578efa5d747aa3ec8bf8
doyoon <doyoun0527@naver.com>
2024-01-18 16:41:36 +0900

    Feat: ReviewServiceImplTest 에 SpringBootTest 어노테이션 작성

--- src/test/java/org/springeel/oneclickrecipe/domain/review/service/impl/ReviewServiceImplTest.java
+++ src/test/java/org/springeel/oneclickrecipe/domain/review/service/impl/ReviewServiceImplTest.java
@@ -1,7 +1,9 @@
 package org.springeel.oneclickrecipe.domain.review.service.impl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ReviewServiceImplTest {
 
     @Test
