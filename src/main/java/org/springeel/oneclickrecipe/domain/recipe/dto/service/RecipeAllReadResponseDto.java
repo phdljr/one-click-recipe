08e4d3d10194485bb517adabe11a6d891993662c
pangseon <chaosen0521@gmail.com>
2024-01-18 16:23:49 +0900

    feature: 레시피 전체조회를 위한 response dto 생성 및 parameter 추가

---
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/dto/service/RecipeAllReadResponseDto.java
@@ -0,0 +1,10 @@
+package org.springeel.oneclickrecipe.domain.recipe.dto.service;
+
+public record RecipeAllReadResponseDto(
+    String title,
+    String intro,
+    Byte serving,
+    String videoPath
+) {
+
+}
