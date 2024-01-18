c5dfeaf51eba3949980130bdbe47e8bf5b5d84a6
pangseon <chaosen0521@gmail.com>
2024-01-18 23:13:42 +0900

    feature: parameter  -> RecipeFoodReadResponseDto 타입 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/recipefood/mapper/service/RecipeFoodEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipefood/mapper/service/RecipeFoodEntityMapper.java
@@ -1,9 +1,12 @@
 package org.springeel.oneclickrecipe.domain.recipefood.mapper.service;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.springeel.oneclickrecipe.domain.food.entity.Food;
+import org.springeel.oneclickrecipe.domain.food.entity.UnitType;
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodReadResponseDto;
 import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipefood.entity.RecipeFood;
 
@@ -13,5 +16,9 @@ public interface RecipeFoodEntityMapper {
 
     RecipeFood toRecipeFood(RecipeFoodCreateServiceRequestDto requestDto, Food food,
         Recipe recipe);
+
+    RecipeFoodReadResponseDto toReadRecipeFood(String name, Short amount, Float price,
+        UnitType unit
+    );
 
 }
