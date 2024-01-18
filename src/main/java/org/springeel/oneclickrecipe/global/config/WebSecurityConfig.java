e4c58323c94704c5b68aab461a7fa71eb51fcfff
pangseon <chaosen0521@gmail.com>
2024-01-19 05:12:48 +0900

    feature: /api/v1/recipes/{recipeId}/processes 인가 작업 추가

--- src/main/java/org/springeel/oneclickrecipe/global/config/WebSecurityConfig.java
+++ src/main/java/org/springeel/oneclickrecipe/global/config/WebSecurityConfig.java
@@ -80,6 +80,7 @@ public class WebSecurityConfig {
                 .requestMatchers("/api/v1/users/signup").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/recipes").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/recipes/{recipeId}/reviews").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/recipes/{recipeId}/processes").permitAll()
                 .anyRequest().authenticated()
         );
 
