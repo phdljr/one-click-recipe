6621b2b7af2ebd03f128ccd54fd8843efdd1930c
doyoon <doyoun0527@naver.com>
2024-01-18 13:03:52 +0900

    Feat: ReviewService 수정(List 추가)

--- src/main/java/org/springeel/oneclickrecipe/domain/review/service/ReviewService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/review/service/ReviewService.java
@@ -1,8 +1,11 @@
 package org.springeel.oneclickrecipe.domain.review.service;
 
 import org.springeel.oneclickrecipe.domain.review.dto.service.ReviewCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.review.dto.service.ReviewReadResponseDto;
 import org.springeel.oneclickrecipe.domain.review.dto.service.ReviewUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
+
+import java.util.List;
 
 public interface ReviewService {
     void createReview(User user, ReviewCreateServiceRequestDto controllerRequestDto, Long recipeId);
