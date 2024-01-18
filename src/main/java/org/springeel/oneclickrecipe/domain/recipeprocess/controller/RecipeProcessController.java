0869e9663b76dd479eaf2389aee5b3078a1c3832
pangseon <chaosen0521@gmail.com>
2024-01-19 06:22:51 +0900

    refactor: readAllRecipeProcess -> readRecipeProcess 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/controller/RecipeProcessController.java
@@ -3,17 +3,14 @@ package org.springeel.oneclickrecipe.domain.recipeprocess.controller;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
-import org.springeel.oneclickrecipe.domain.recipe.repository.RecipeRepository;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.controller.RecipeProcessCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.controller.RecipeProcessUpdateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessCreateServiceRequestDto;
-import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessReadServiceResponseDto;
+import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessReadResponseDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.mapper.dto.RecipeProcessDtoMapper;
 import org.springeel.oneclickrecipe.domain.recipeprocess.service.RecipeProcessService;
 import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
-import org.springeel.oneclickrecipe.global.util.S3Provider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,7 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -76,11 +72,11 @@ public class RecipeProcessController {
     }
 
     @GetMapping("/{recipeId}/recipe-processes")
-    public ResponseEntity<List<RecipeProcessReadServiceResponseDto>> readAll(
+    public ResponseEntity<List<RecipeProcessReadResponseDto>> readAll(
         @PathVariable Long recipeId
     ) {
-        List<RecipeProcessReadServiceResponseDto> processes =
-            processService.readAllRecipeProcess(recipeId);
+        List<RecipeProcessReadResponseDto> processes =
+            processService.readRecipeProcess(recipeId);
 
         return ResponseEntity.status(HttpStatus.OK).body(processes);
     }
