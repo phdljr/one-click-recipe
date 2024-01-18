b44854eb9806c9549603c1e148a68862d11ef57f
doyoon <doyoun0527@naver.com>
2024-01-18 16:40:34 +0900

    Feat: ReviewServiceImplTest 코드 초기작성

---
+++ src/test/java/org/springeel/oneclickrecipe/domain/review/service/impl/ReviewServiceImplTest.java
@@ -0,0 +1,10 @@
+package org.springeel.oneclickrecipe.domain.review.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+class ReviewServiceImplTest {
+
+    @Test
+    void getReviews() {
+    }
+}
