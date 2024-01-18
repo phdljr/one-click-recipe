07dfb9fb0968d6c229311489dbfc57215aa7622d
pangseon <chaosen0521@gmail.com>
2024-01-19 05:10:25 +0900

    feature: List<RecipeProcess> -> List<RecipeProcessReadServiceResponseDto> 반환 기능 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/mapper/entity/RecipeProcessEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipeprocess/mapper/entity/RecipeProcessEntityMapper.java
@@ -1,9 +1,11 @@
 package org.springeel.oneclickrecipe.domain.recipeprocess.mapper.entity;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
 import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessReadServiceResponseDto;
 import org.springeel.oneclickrecipe.domain.recipeprocess.entity.RecipeProcess;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
@@ -12,5 +14,8 @@ public interface RecipeProcessEntityMapper {
 
     RecipeProcess toRecipeProcess(RecipeProcessCreateServiceRequestDto requestDto, String imageUrl,
         Recipe recipe);
+
+    List<RecipeProcessReadServiceResponseDto> toReadRecipeProcess(
+        List<RecipeProcess> recipeProcesses);
 
 }
