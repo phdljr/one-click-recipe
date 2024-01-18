a5702731b097dbee935d535ef963aed5e10e292e
gyutae <qoxowkd0716@gmail.com>
2024-01-18 15:49:05 +0900

    Feat: recipeFood오 food에 있는 필드값 매핑

--- src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/orderdetail/mapper/OrderDetailMapper.java
@@ -1,11 +1,16 @@
 package org.springeel.oneclickrecipe.domain.orderdetail.mapper;
 
 import org.mapstruct.Mapper;
-import org.springeel.oneclickrecipe.domain.orderdetail.dto.OrderDetailDto;
+import org.mapstruct.Mapping;
+import org.springeel.oneclickrecipe.domain.cart.entity.Cart;
+import org.springeel.oneclickrecipe.domain.order.entity.Order;
 import org.springeel.oneclickrecipe.domain.orderdetail.entity.OrderDetail;
 
 @Mapper(componentModel = "spring")
 public interface OrderDetailMapper {
 
-    OrderDetailDto toOrderDetailDto(OrderDetail orderDetail);
+    @Mapping(source = "recipeFood.food.name", target = "foodName")
+    @Mapping(source = "recipeFood.amount", target = "amount")
+    @Mapping(source = "recipeFood.food.price", target = "price")
+    OrderDetail toOrderDetail(Cart cart, Order order);
 }
