ec89832916934d341912b71e084258d3f45813e8
pangseon <chaosen0521@gmail.com>
2024-01-18 14:34:36 +0900

    refactor: S3 업데이트 될 때 변경 기준을 3가지로 나누어서 재구성하였습니다.

--- src/main/java/org/springeel/oneclickrecipe/global/util/S3Provider.java
+++ src/main/java/org/springeel/oneclickrecipe/global/util/S3Provider.java
@@ -67,24 +67,26 @@ public class S3Provider {
 
     public String updateImage(String imageName, String folderName, MultipartFile multipartFile)
         throws IOException {
-        if (imageName == null) {
-            String NewImage = originalFileName(multipartFile);
-            imageName = url + folderName + SEPARATOR + NewImage;
-            String saveImageUrl = folderName + SEPARATOR + NewImage;
-            ObjectMetadata metadata = setObjectMetadata(multipartFile);
-            amazonS3.putObject(bucket, saveImageUrl, multipartFile.getInputStream(), metadata);
-        }
-        imageName = imageName.replace(url, "");
-        imageName = imageName.substring(imageName.lastIndexOf("/"));
-        deleteImage(folderName + imageName);
-        if (multipartFile.isEmpty()) {
-            imageName = null;
+        // S3에 대한 정보저장이나 변경이 없을 경우
+        if (imageName == null && multipartFile.isEmpty()) {
+            return null;
         } else {
+            // S3에 대한 정보가 DB에 저장되었지만 해당 내용을 삭제하고 싶을 때
+            if (multipartFile.isEmpty()) {
+                imageName = imageName.replace(url, "");
+                imageName = imageName.substring(imageName.lastIndexOf("/"));
+                deleteImage(folderName + imageName);
+                return null;
+            }
+            // 이미지 내용을 변경하고 싶거나 또는 유지하고 싶을 때
             String NewImage = originalFileName(multipartFile);
             imageName = url + folderName + SEPARATOR + NewImage;
             String saveImageUrl = folderName + SEPARATOR + NewImage;
             ObjectMetadata metadata = setObjectMetadata(multipartFile);
             amazonS3.putObject(bucket, saveImageUrl, multipartFile.getInputStream(), metadata);
+            imageName = imageName.replace(url, "");
+            imageName = imageName.substring(imageName.lastIndexOf("/"));
+            deleteImage(folderName + imageName);
         }
         return imageName;
     }
