768a2a4f9c110c1e523f3c7c6b8435ea0967b335
pangseon <chaosen0521@gmail.com>
2024-01-19 06:18:11 +0900

    feature: 레시피 단건조회를 위한 parameters -> RecipeReadResponseDto 변환

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/mapper/entity/RecipeEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/mapper/entity/RecipeEntityMapper.java
@@ -5,7 +5,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeAllReadResponseDto;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.recipe.dto.service.RecipeReadResponseDto;
 import org.springeel.oneclickrecipe.domain.recipe.entity.Recipe;
+import org.springeel.oneclickrecipe.domain.recipefood.dto.service.RecipeFoodReadResponseDto;
+import org.springeel.oneclickrecipe.domain.recipeprocess.dto.service.RecipeProcessReadResponseDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 
 @Mapper(componentModel = ComponentModel.SPRING)
@@ -14,5 +17,8 @@ public interface RecipeEntityMapper {
     Recipe toRecipe(RecipeCreateServiceRequestDto requestDto, User user, String folderName);
 
     List<RecipeAllReadResponseDto> toRecipeAllRead(List<Recipe> recipe);
+
+    RecipeReadResponseDto toRecipeRead(Recipe recipe, List<RecipeFoodReadResponseDto> recipe_foods,
+        List<RecipeProcessReadResponseDto> recipe_processes);
 
 }
