f30ffddc56ba0ab4e373f7659b0476a32b73b3e1
pangseon <chaosen0521@gmail.com>
2024-01-20 01:37:49 +0900

    refactor: 레시피 생성 시 파일 이미지 추가 메서드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/recipe/service/RecipeService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/recipe/service/RecipeService.java
@@ -1,15 +1,18 @@
 package org.springeel.oneclickrecipe.domain.recipe.service;
 
+import java.io.IOException;
 import java.util.List;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.request.RecipeCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.request.RecipeUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.response.RecipeAllReadResponseDto;
 import org.springeel.oneclickrecipe.domain.recipe.dto.service.response.RecipeReadResponseDto;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface RecipeService {
 
-    void createRecipe(RecipeCreateServiceRequestDto controllerRequestDto, User user);
+    void createRecipe(RecipeCreateServiceRequestDto controllerRequestDto, User user,
+        MultipartFile multipartFile) throws IOException;
 
     void deleteRecipe(Long recipeId, User user);
 
