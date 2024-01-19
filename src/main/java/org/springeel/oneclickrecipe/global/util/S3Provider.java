35be1115b8bb500d72ac34e48babaa56f05f2800
pangseon <chaosen0521@gmail.com>
2024-01-19 22:59:13 +0900

    refactor: S3 업데이트 시 기존에 있는 파일 삭제 로직 수정

--- src/main/java/org/springeel/oneclickrecipe/global/util/S3Provider.java
+++ src/main/java/org/springeel/oneclickrecipe/global/util/S3Provider.java
@@ -76,17 +76,18 @@ public class S3Provider {
                 imageName = imageName.replace(url, "");
                 imageName = imageName.substring(imageName.lastIndexOf("/"));
                 deleteImage(folderName + imageName);
-                return null;
+                imageName = null;
+                return imageName;
             }
-            // 이미지 내용을 변경하고 싶거나 또는 유지하고 싶을 때
+            imageName = imageName.replace(url, "");
+            imageName = imageName.substring(imageName.lastIndexOf("/"));
+            deleteImage(folderName + imageName);// 이미지 내용을 변경하고 싶거나 또는 유지하고 싶을 때
             String NewImage = originalFileName(multipartFile);
             imageName = url + folderName + SEPARATOR + NewImage;
             String saveImageUrl = folderName + SEPARATOR + NewImage;
             ObjectMetadata metadata = setObjectMetadata(multipartFile);
             amazonS3.putObject(bucket, saveImageUrl, multipartFile.getInputStream(), metadata);
-            imageName = imageName.replace(url, "");
-            imageName = imageName.substring(imageName.lastIndexOf("/"));
-            deleteImage(folderName + imageName);
+
         }
         return imageName;
     }
