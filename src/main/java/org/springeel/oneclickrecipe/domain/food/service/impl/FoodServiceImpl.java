f6826965db92a3bf2d88a4ccc16ccdd207f43872
pangseon <chaosen0521@gmail.com>
2024-01-23 04:16:09 +0900

    feature: 식재료 삭제 기능 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/food/service/impl/FoodServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/food/service/impl/FoodServiceImpl.java
@@ -3,6 +3,8 @@ package org.springeel.oneclickrecipe.domain.food.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.food.dto.service.FoodCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.food.entity.Food;
+import org.springeel.oneclickrecipe.domain.food.exception.FoodErrorCode;
+import org.springeel.oneclickrecipe.domain.food.exception.NotFoundFoodException;
 import org.springeel.oneclickrecipe.domain.food.mapper.service.FoodEntityMapper;
 import org.springeel.oneclickrecipe.domain.food.repository.FoodRepository;
 import org.springeel.oneclickrecipe.domain.food.service.FoodService;
@@ -19,11 +21,23 @@ public class FoodServiceImpl implements FoodService {
     private final FoodRepository foodRepository;
     private final UserRepository userRepository;
     private final FoodEntityMapper foodEntityMapper;
-    
-    public void CreateFood(FoodCreateServiceRequestDto requestDto, User user) {
-        userRepository.findByRole(user.getRole())
-            .orElseThrow(() -> new NotFoundUserException(UserErrorCode.NOT_FOUND_USER));
+
+    public void createFood(FoodCreateServiceRequestDto requestDto, User user) {
+        User admin = finduser(user);
         Food food = foodEntityMapper.toFood(requestDto);
         foodRepository.save(food);
+    }
+
+    public void deleteFood(User user, Long id) {
+        User admin = finduser(user);
+        Food food = foodRepository.findById(id)
+            .orElseThrow(() -> new NotFoundFoodException(FoodErrorCode.NOT_FOUND_FOOD));
+        foodRepository.delete(food);
+    }
+
+    private User finduser(User user) {
+        User admin = userRepository.findByRole(user.getRole())
+            .orElseThrow(() -> new NotFoundUserException(UserErrorCode.NOT_FOUND_USER));
+        return admin;
     }
 }
