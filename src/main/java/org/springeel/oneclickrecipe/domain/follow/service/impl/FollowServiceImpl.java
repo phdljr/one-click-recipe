a1c9dc5a31765e70a2b43a34ae6e60b346e55475
doyoon <doyoun0527@naver.com>
2024-01-22 21:56:51 +0900

    Feat: 메서드 이름 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
@@ -2,6 +2,7 @@ package org.springeel.oneclickrecipe.domain.follow.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springeel.oneclickrecipe.domain.follow.entity.Follow;
+import org.springeel.oneclickrecipe.domain.follow.exception.AlreadyExistFollowException;
 import org.springeel.oneclickrecipe.domain.follow.exception.FollowErrorCode;
 import org.springeel.oneclickrecipe.domain.follow.exception.NotFoundFollowException;
 import org.springeel.oneclickrecipe.domain.follow.repository.FollowRepository;
@@ -19,6 +20,11 @@ public class FollowServiceImpl implements FollowService {
     public void create(User user, Long userId) {
         Follow follow = followRepository.findByFollowingId(userId)
             .orElseThrow(() -> new NotFoundFollowException(FollowErrorCode.NOT_FOUND_FOLLOW));
+
+        if (followRepository.existsByUserIdAndFollowingId(user.getId(), userId)) {
+            throw new AlreadyExistFollowException(
+                FollowErrorCode.ALREADY_EXIST_FOLLOW);
+        }
 
 
     }
