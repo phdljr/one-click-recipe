96c1efd51ca9e2d2e63f6026ca133402b7377134
pangseon <chaosen0521@gmail.com>
2024-01-23 05:01:55 +0900

    feature: 식재료 업데이트 메서드 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/food/service/FoodService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/food/service/FoodService.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.food.service;
 
 import org.springeel.oneclickrecipe.domain.food.dto.service.FoodCreateServiceRequestDto;
+import org.springeel.oneclickrecipe.domain.food.dto.service.FoodUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.food.repository.FoodRepository;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,6 @@ public interface FoodService {
     void createFood(FoodCreateServiceRequestDto requestDto, User user);
 
     void deleteFood(User user, Long id);
+
+    void updateFood(User user, Long id, FoodUpdateServiceRequestDto requestDto);
 }
