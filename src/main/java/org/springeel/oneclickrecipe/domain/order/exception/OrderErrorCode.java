6d55aa3b6fb2102e75769a3680bd44d3a16aca7b
gyutae <qoxowkd0716@gmail.com>
2024-01-22 22:44:36 +0900

    Refactor: 필요없는 예외처리 삭제

--- src/main/java/org/springeel/oneclickrecipe/domain/order/exception/OrderErrorCode.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/order/exception/OrderErrorCode.java
@@ -8,14 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum OrderErrorCode implements ErrorCode {
-
     // 400
-    ALREADY_EXIST_ORDER(HttpStatus.BAD_REQUEST, "이미 주문내역이 존재합니다."),
     ALREADY_PROCESS_ORDER(HttpStatus.BAD_REQUEST, "이미 처리된 주문입니다."),
-
-    // 403
-    FORBIDDEN_ACCESS_ORDER(HttpStatus.FORBIDDEN, "주문내역에 접근할 수 없습니다."),
-
     // 404
     NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "주문내역를 찾을 수 없습니다.");
 
