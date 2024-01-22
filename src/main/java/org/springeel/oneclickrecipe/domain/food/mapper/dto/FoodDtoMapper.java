e883ef9340fec65166439831e568713a1c30cfab
pangseon <chaosen0521@gmail.com>
2024-01-22 21:50:49 +0900

    feature: Mapper 어노테이션 추가

--- src/main/java/org/springeel/oneclickrecipe/domain/food/mapper/dto/FoodDtoMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/food/mapper/dto/FoodDtoMapper.java
@@ -1,7 +1,12 @@
 package org.springeel.oneclickrecipe.domain.food.mapper.dto;
 
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+import org.mapstruct.Mapper;
 import org.springeel.oneclickrecipe.domain.food.dto.controller.FoodCreateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.food.dto.service.FoodCreateServiceRequestDto;
+
+@Mapper(componentModel = SPRING)
 
 public interface FoodDtoMapper {
 
--- src/main/java/org/springeel/oneclickrecipe/domain/food/mapper/service/FoodEntityMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/food/mapper/service/FoodEntityMapper.java
@@ -1,7 +1,12 @@
 package org.springeel.oneclickrecipe.domain.food.mapper.service;
 
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+import org.mapstruct.Mapper;
 import org.springeel.oneclickrecipe.domain.food.dto.service.FoodCreateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.food.entity.Food;
+
+@Mapper(componentModel = SPRING)
 
 public interface FoodEntityMapper {
 
