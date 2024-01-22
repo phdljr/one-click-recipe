7d8f26d431f0746558e95ce5ec93d33a01e1b69b
doyoon <doyoun0527@naver.com>
2024-01-22 21:56:47 +0900

    Feat: 메서드 이름 변경

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/repository/FollowRepository.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/repository/FollowRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface FollowRepository extends JpaRepository<Follow, FollowId> {
     Optional<Follow> findByFollowingId(Long userId);
 
-    boolean existByUserAndUserId(Long id, Long userId);
+    boolean existsByUserIdAndFollowingId(Long id, Long userId);
 }
