1b1ea1aef81ba2fafdce7d08702c28bd74132322
doyoon <doyoun0527@naver.com>
2024-01-24 00:58:07 +0900

    Feat: userRepository 주입

--- src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/follow/service/impl/FollowServiceImpl.java
@@ -1,13 +1,13 @@
 package org.springeel.oneclickrecipe.domain.follow.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springeel.oneclickrecipe.domain.follow.entity.Follow;
-import org.springeel.oneclickrecipe.domain.follow.exception.AlreadyExistFollowException;
+import org.springeel.oneclickrecipe.domain.follow.exception.AlreadyExistsFollowException;
 import org.springeel.oneclickrecipe.domain.follow.exception.FollowErrorCode;
 import org.springeel.oneclickrecipe.domain.follow.exception.NotFoundFollowException;
 import org.springeel.oneclickrecipe.domain.follow.repository.FollowRepository;
 import org.springeel.oneclickrecipe.domain.follow.service.FollowService;
 import org.springeel.oneclickrecipe.domain.user.entity.User;
+import org.springeel.oneclickrecipe.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -15,14 +15,16 @@ import org.springframework.stereotype.Service;
 public class FollowServiceImpl implements FollowService {
 
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
 
     @Override
     public void create(User user, Long followingId) {
-        Follow follow = followRepository.findByFollowingId(followingId)
+        // Follow follow = followRepository.findByFollowingId(followingId)
+        User user1 = userRepository.findById(followingId)
             .orElseThrow(() -> new NotFoundFollowException(FollowErrorCode.NOT_FOUND_FOLLOW));
 
         if (followRepository.existsByUserIdAndFollowingId(user.getId(), followingId)) {
-            throw new AlreadyExistFollowException(
+            throw new AlreadyExistsFollowException(
                 FollowErrorCode.ALREADY_EXIST_FOLLOW);
         }
 
