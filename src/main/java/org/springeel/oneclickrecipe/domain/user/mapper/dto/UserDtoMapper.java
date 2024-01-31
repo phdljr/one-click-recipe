88f11cd7ce3e4e853aa2a13ce1d390357d8b6967
doyoon <doyoun0527@naver.com>
2024-01-31 17:02:05 +0900

    Feat: mapper 구현

--- src/main/java/org/springeel/oneclickrecipe/domain/user/mapper/dto/UserDtoMapper.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/user/mapper/dto/UserDtoMapper.java
@@ -1,15 +1,21 @@
 package org.springeel.oneclickrecipe.domain.user.mapper.dto;
 
-import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
-
 import org.mapstruct.Mapper;
+import org.springeel.oneclickrecipe.domain.user.dto.controller.NicknameUpdateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.controller.UserLoginControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.controller.UserSignUpControllerRequestDto;
+import org.springeel.oneclickrecipe.domain.user.dto.service.request.NicknameUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserLoginServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserSignUpServiceRequestDto;
 
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
 @Mapper(componentModel = SPRING)
 public interface UserDtoMapper {
+
+    NicknameUpdateServiceRequestDto toNicknameUpdateServiceRequestDto(
+        NicknameUpdateControllerRequestDto controllerRequestDto);
+
 
     UserSignUpServiceRequestDto toUserSignUpServiceRequestDto(
         UserSignUpControllerRequestDto controllerRequestDto);
