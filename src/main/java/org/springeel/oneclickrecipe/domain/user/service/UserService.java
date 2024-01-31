bb046d0bfe7ee96260da15dd0a2f7050b7c243c5
doyoon <doyoun0527@naver.com>
2024-01-31 17:52:41 +0900

    Feat: updatePassword 로직추가

--- src/main/java/org/springeel/oneclickrecipe/domain/user/service/UserService.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package org.springeel.oneclickrecipe.domain.user.service;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springeel.oneclickrecipe.domain.user.dto.service.PasswordUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.NicknameUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserLoginServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserSignUpServiceRequestDto;
@@ -17,4 +18,6 @@ public interface UserService {
     UserLoginResponseDto refreshAccessToken(String refreshToken, User user, HttpServletResponse httpServletResponse);
 
     void updateNickname(Long userId, User user, NicknameUpdateServiceRequestDto serviceRequestDto);
+
+    void updatePassword(Long userId, User user, PasswordUpdateServiceRequestDto serviceRequestDto);
 }
