6fe48757ae399cdf96bed37e329b8fba6c5566c9
doyoon <doyoun0527@naver.com>
2024-01-31 17:01:42 +0900

    Feat: Update API 작성

--- src/main/java/org/springeel/oneclickrecipe/domain/user/controller/UserController.java
+++ src/main/java/org/springeel/oneclickrecipe/domain/user/controller/UserController.java
@@ -4,8 +4,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springeel.oneclickrecipe.domain.user.dto.controller.NicknameUpdateControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.controller.UserLoginControllerRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.controller.UserSignUpControllerRequestDto;
+import org.springeel.oneclickrecipe.domain.user.dto.service.request.NicknameUpdateServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserLoginServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.request.UserSignUpServiceRequestDto;
 import org.springeel.oneclickrecipe.domain.user.dto.service.response.UserLoginResponseDto;
@@ -15,11 +17,7 @@ import org.springeel.oneclickrecipe.global.security.UserDetailsImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,6 +27,7 @@ public class UserController {
 
     private final UserDtoMapper userDtoMapper;
     private final UserService userService;
+
 
     @PostMapping("/signup")
     public ResponseEntity<Void> singUp(
@@ -60,5 +59,17 @@ public class UserController {
         UserLoginResponseDto responseDto = userService.refreshAccessToken(refreshToken,
             userDetails.user(), httpServletResponse);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @PutMapping("/{userId}/nickname") //닉네임변경(수정)
+    public ResponseEntity<?> update(
+        @PathVariable(name = "userId") Long userId,
+        @Valid @RequestBody NicknameUpdateControllerRequestDto controllerRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        NicknameUpdateServiceRequestDto serviceRequestDto =
+            userDtoMapper.toNicknameUpdateServiceRequestDto(controllerRequestDto);
+        userService.updateNickname(userId, userDetails.user(), serviceRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
